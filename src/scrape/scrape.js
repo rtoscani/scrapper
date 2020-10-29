6a29ececf756a4765bd1045f2419f4846bcc5a8c
Renzo Toscani <renzo.toscani@gmail.com>
2020-10-29 20:02:41 -0300

    scrape in paralell promises

--- src/scrape/scrape.js
+++ src/scrape/scrape.js
@@ -9,25 +9,31 @@ module.exports = {
         console.info('Scrapping stores...');
         let results = [];
 
+        let promises = [];
+
         // Iterate over the stores to load to scrape their products
         for (let store of Stores.filter(store => store.enabled)) {
             console.info('Scrapping site: ' + store.name);
             
-            let result = await scrapeUtils.scrapeSite(search.text, store)
+            promises.push(scrapeUtils.scrapeSite(search.text, store)
                 .then(response => { return response; })
                 .catch(err => { 
                     console.error("Unable to scrape site: " + err);
                     return [];
-                });
+                }));
+        }
 
+        await Promise.all(promises).then(result => {
             // Filter products using the regex
-            for (let product of result) {
-                let regex = new RegExp(search.shouldContain, "i");
-                if (regex.test(product.name)) {
-                    results.push(product);
+            for (let products of result) {
+                for (let product of products) {
+                    let regex = new RegExp(search.shouldContain, "i");
+                    if (regex.test(product.name)) {
+                        results.push(product);
+                    }
                 }
             }
-        }
+        });
 
         console.log(results);
         console.info('Scrapping finished. Found ' + results.length + ' products in stock');
